bb022c4b5d8ca6cf5f15231b007af29097a55fba
Kerneld82 <kerneld@naver.com>
2024-04-19 15:21:11 +0900

    style: Add Whitespace to split_odd_even

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ def check_available():
 def split_odd_even(nums=[1, 2, 3, 4]):
     odds = []
     evens = []
+
     for i in nums:
         if i % 2 == 0:
             evens.append(i)
         else:
             odds.append(i)
+
     return (odds, evens)
 
 if __name__ == '__main__':
